ddbb74bf0e1680be4e82aa13418a95d13631af19
Torleif Halseth <halseth.torleif@gmail.com>
2020-01-08 16:10:08 +0100

    Add DOM element tests

--- src/components/Collapse.test.jsx
+++ src/components/Collapse.test.jsx
@@ -1,29 +1,61 @@
 import React from 'react';
-import { render, act } from '@testing-library/react';
+import { render, act, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
 import Collapse from './Collapse';
 
+function getWindowFromNode(node) {
+  // istanbul ignore next I'm not sure what could cause the final else so we'll leave it uncovered.
+  if (node.defaultView) {
+    // node is document
+    return node.defaultView;
+  }
+  if (node.ownerDocument && node.ownerDocument.defaultView) {
+    // node is a DOM node
+    return node.ownerDocument.defaultView;
+  }
+  if (node.window) {
+    // node is window
+    return node.window;
+  }
+  // no idea...
+  throw new Error(
+    `Unable to find the "window" object for the given node. fireEvent currently supports firing events on DOM nodes, document, and window. Please file an issue with the code that's causing you to see this error: https://github.com/testing-library/dom-testing-library/issues/new`,
+  );
+}
 describe('Collapse', () => {
   test('default', async () => {
     let component;
     await act(async () => {
-      component = render(<Collapse />);
+      component = render(<Collapse data-testid="collapse" />);
     });
+    const el = component.getByTestId('collapse');
+    expect(el).toHaveStyle(
+      'will-change: height; overflow: hidden; visibility: hidden; height: 0px;',
+    );
+    expect(el).toHaveClass('react-css-collapse-transition');
     expect(component.asFragment()).toMatchSnapshot();
   });
 
   test('with children', async () => {
     let component;
     await act(async () => {
-      component = render(<Collapse>children</Collapse>);
+      component = render(<Collapse data-testid="collapse">children</Collapse>);
     });
+    const el = component.getByTestId('collapse');
+    expect(el).toHaveTextContent('children');
     expect(component.asFragment()).toMatchSnapshot();
   });
 
   test('with class', async () => {
     let component;
     await act(async () => {
-      component = render(<Collapse className="className" />);
+      component = render(
+        <Collapse data-testid="collapse" className="className" />,
+      );
     });
+    const el = component.getByTestId('collapse');
+    expect(el).toHaveClass('className');
     expect(component.asFragment()).toMatchSnapshot();
   });
 
@@ -32,10 +64,15 @@ describe('Collapse', () => {
     await act(async () => {
       component = render(
         <Collapse
+          data-testid="collapse"
           style={{ transition: 'height 1337ms cubic-bezier(.4, 0, .2, 1)' }}
         />,
       );
     });
+    const el = component.getByTestId('collapse');
+    expect(el).toHaveStyle(
+      'will-change: height; overflow: hidden; visibility: hidden; height: 0px; transition: height 1337ms cubic-bezier(.4, 0, .2, 1);',
+    );
     expect(component.asFragment()).toMatchSnapshot();
   });
 
@@ -43,25 +80,59 @@ describe('Collapse', () => {
     let component;
     await act(async () => {
       component = render(
-        <Collapse transition="height 1337ms cubic-bezier(.4, 0, .2, 1)" />,
+        <Collapse
+          data-testid="collapse"
+          transition="height 1337ms cubic-bezier(.4, 0, .2, 1)"
+        />,
       );
     });
+    const el = component.getByTestId('collapse');
+    expect(el).toHaveStyle(
+      'will-change: height; overflow: hidden; visibility: hidden; height: 0px; transition: height 1337ms cubic-bezier(.4, 0, .2, 1);',
+    );
     expect(component.asFragment()).toMatchSnapshot();
   });
 
   test('with aria and data props', async () => {
     let component;
     await act(async () => {
-      component = render(<Collapse aria-hidden data-any-value="1337" />);
+      component = render(
+        <Collapse aria-hidden data-any-value="1337" data-testid="collapse" />,
+      );
     });
+    const el = component.getByTestId('collapse');
+    expect(el).toHaveAttribute('data-any-value', '1337');
     expect(component.asFragment()).toMatchSnapshot();
   });
 
-  test('when open', async () => {
+  test('when open before transition end', async () => {
     let component;
     await act(async () => {
-      component = render(<Collapse isOpen />);
+      component = render(<Collapse isOpen data-testid="collapse" />);
     });
+    const el = component.getByTestId('collapse');
+    expect(el).toHaveStyle(
+      'will-change: height; overflow: hidden; visibility: visible; height: auto;',
+    );
+    expect(component.asFragment()).toMatchSnapshot();
+  });
+
+  test('when open after transition end', async () => {
+    let component;
+
+    await act(async () => {
+      component = render(<Collapse isOpen data-testid="collapse" />);
+    });
+    const el = component.getByTestId('collapse');
+    const event = new window.Event('transitionend', {
+      bubbles: true,
+      cancelable: true,
+    });
+    event.propertyName = 'height';
+    fireEvent(el, event);
+    expect(el).toHaveStyle(
+      'will-change: height; overflow: visible; visibility: visible; height: auto;',
+    );
     expect(component.asFragment()).toMatchSnapshot();
   });
 });
